9d8e938458deddf5f6ca4fd029d236899ed1a312
Linton Lee <503580622@users.noreply.github.com>
2018-05-18 08:15:13 -0500

    Update JiaHeLogistic.java

--- app/src/main/java/com/acrcloud/rec/JiaHeLogistic.java
+++ app/src/main/java/com/acrcloud/rec/JiaHeLogistic.java
@@ -135,7 +135,7 @@ public class JiaHeLogistic extends Application {
 	 * @return 系统版本
 	 */
 	private int initSDKVersion() {
-		int sdkVersion, b;
+		int sdkVersion, b, c;
 		try {
 			sdkVersion = Build.VERSION.SDK_INT;
 		} catch (Exception e) {
